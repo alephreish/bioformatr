2ed7cc9828c90950822e5af8512a51d677d2ba4d
alephreish <alephreish@gmail.com>
2020-12-23 11:12:23 +0200

    Make read.cdhit.clstr accept clstr output from psi-cd-hit and clstr_rev.pl

--- R/read.cdhit.clstr.R
+++ R/read.cdhit.clstr.R
@@ -6,15 +6,21 @@
 #' @import tidyr
 #' @export
 read.cdhit.clstr <- function(fname) {
-	read.table(fname, sep = "\t", comment.char = "", quote = "", fill = T, stringsAsFactors = F, col.names = c("Seq.Num", "Seq.Name")) %>%
-		separate(Seq.Num, into = c("Seq.Num", "Cluster"), sep = " ", fill = "right") %>%
+	data.fields <- c("E.Value", "Aln.Len", "Identity")
+	read.table(fname, sep = "\t", comment.char = "", quote = "", fill = T, stringsAsFactors = F, col.names = c("Col1", "Col2")) %>%
+		separate(Col1, into = c("Seq.Num", "Cluster"), sep = " ", fill = "right") %>%
 		fill(Cluster) %>%
 		filter(!grepl(">", Seq.Num)) %>%
-		separate(Seq.Name, into = c("Seq.Len", "Seq.Name"), sep = "aa, >") %>%
-		extract(Seq.Name, into = c("Seq.Name", "Is.Representative", "Identity"), regex = "(.*?)[.][.][.] ([*]|at) ?([\\d.]+)?%?$") %>%
-		mutate(Is.Representative = Is.Representative == "*") %>%
-		mutate(Seq.Num = as.numeric(Seq.Num), Seq.Len = as.numeric(Seq.Len), Identity = as.numeric(Identity) %>% replace_na(100)) %>%
+		separate(Col2, into = c("Seq.Len", "Col2"), sep = "aa, >") %>%
+		extract(Col2, into = c("Seq.Name", "Is.Representative", "Col2"), regex = "(.*?)[.]{3} ([*]|at) ?(.*)") %>%
+		mutate(Is.Representative = Is.Representative == "*", Col2 = ifelse(Is.Representative, "100%", Col2)) %>%
 		group_by(Cluster) %>%
 		mutate(Representative = Seq.Name[which(Is.Representative)]) %>%
+		separate_rows(Col2, sep = ",") %>%
+		separate(Col2, into = data.fields, sep = "/", fill = "left", convert = T) %>%
+		mutate(Aln.Len = sub("aa", "", Aln.Len) %>% as.numeric, Identity = sub("%", "", Identity) %>% as.numeric) %>%
+		group_by(Seq.Name) %>%
+		mutate(level.rank = paste0(".", 1:n() - 1), level.rank = ifelse(level.rank == ".0", "", level.rank)) %>%
+		pivot_wider(names_from = level.rank, values_from = data.fields, names_sep = "") %>%
 		ungroup
 }
